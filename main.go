73206a48bcc0507ce16961373025b75b0e275fb3
Daniel Ntege <danientege785@gmail.com>
2025-08-02 23:27:50 +0300

    commit

--- main.go
+++ main.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-"fmt"
-"os"
+  "fmt"
+  "os"
 
-"github.com/jdk-manager/cmd"
+  "github.com/jdk-manager/cmd"
 )
 
 func main() {
-fmt.Println("JDK Manager is starting...") // Temporary debug line
-if err := cmd.Execute(); err != nil {
-	fmt.Fprintf(os.Stderr, "Error: %v\n", err)
-	os.Exit(1)
-}
+  if err := cmd.Execute(); err != nil {
+  	fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+  	os.Exit(1)
+  }
 }
